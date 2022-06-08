3638224c0fa0a5dcb81f1b9c640fa2370d5a81be
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-06-08 12:33:32 +0300

    add

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,59 +1,46 @@
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getSelectedCategory } from "../store/Categories";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
 
 function Categories(props) {
   const [value, setValue] = useState(0);
   const getActiveCategory = props.getSelectedCategory;
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+
   useEffect(() => {
     getActiveCategory(value + 1);
   }, [value, getActiveCategory]);
   return (
     <div>
       <h3>Browse Our Categories</h3>
-
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
+      <ButtonGroup variant="text" aria-label="text button group">
+        <Button
+          onClick={() => {
+            setValue(0);
+          }}
         >
-          <Tab label="cars" />
-          <Tab label="food" />
-          <Tab label="phones" />
-        </Tabs>
-      </Box>
-
-      <TabPanel value={value} index={1}></TabPanel>
-      <TabPanel value={value} index={2}></TabPanel>
-      <TabPanel value={value} index={3}></TabPanel>
+          cars
+        </Button>
+        <Button
+          onClick={() => {
+            setValue(1);
+          }}
+        >
+          food
+        </Button>
+        <Button
+          onClick={() => {
+            setValue(2);
+          }}
+        >
+          phones
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
