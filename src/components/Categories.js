a30dc90e6ca6e0c8ef99a2e1671a32016d1e269d
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-06-13 08:57:44 +0300

    add

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,5 @@
-
-
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-
-
 
 import { connect, useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
@@ -11,25 +7,22 @@ import { useState, useEffect } from "react";
 import { getSelectedCategory, getRemoteData } from "../store/Categories";
 
 function Categories(props) {
-  const dispatchData = useDispatch(); 
+  const dispatchData = useDispatch();
 
- 
   const { getSelectedCategory } = props;
   const [dispalyName, setdispalyName] = useState(1);
   useEffect(() => {
-    console.log("dispalyName>>>>>>>>>   ", dispalyName);
     getSelectedCategory(dispalyName);
   }, [dispalyName, getSelectedCategory]);
   useEffect(() => {
     dispatchData(getRemoteData());
   }, []);
   const newCategory = useSelector((state) => state.catigory.category);
-  
+
   return (
     <>
       <ButtonGroup variant="text" aria-label="text button group">
         {newCategory.map((category) => {
-          console.log("Onclick : ", category.dispalyName);
           return (
             <Button
               key={category.id}
@@ -49,5 +42,5 @@ function Categories(props) {
 }
 
 const mapStateToPrps = (state) => ({ category: state.catigory });
-const mapDispatchToProps = { getSelectedCategory }; 
+const mapDispatchToProps = { getSelectedCategory };
 export default connect(mapStateToPrps, mapDispatchToProps)(Categories);
